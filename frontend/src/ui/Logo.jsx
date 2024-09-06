19bc3d89e7c7724b34682ad36db1c4d5e54a83b0
Metin Kuran <metinkuran98@gmail.com>
2024-09-06 18:15:20 +0300

    push7

--- frontend/src/ui/Logo.jsx
+++ frontend/src/ui/Logo.jsx
@@ -3,7 +3,7 @@ function Logo() {
     <div className='flex items-center p-2'>
       <img
         className='w-30 ml-10 h-20'
-        src='./src/assets/images/logo.png'
+        src='/images/logo.png'
         alt=''
       />
     </div>
