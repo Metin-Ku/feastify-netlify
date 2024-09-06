76bb3b7a8ab65f4c539b1338e12e96f7c8707105
Metin Kuran <metinkuran98@gmail.com>
2024-09-06 18:00:10 +0300

    push6

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const corsOptions = {
           'http://localhost:4173',
           'http://localhost:4173',
         ]
-      : 'https://feastify-netlify.onrender.com',
+      : 'https://feastify-netlify.vercel.app',
   credentials: true, // Allow cookies to be included in requests
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
